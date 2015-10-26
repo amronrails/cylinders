3756214791fae8566769f9782ebcaccbb7800d3c
amronrails <amre.abdelhady@gmail.com>
2015-10-26 19:10:21 +0200

    hisham

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,7 +1,8 @@
 class BrandsController < ApplicationController
 
   layout "twitter"
-  before_action :confirm_login
+  #TODO.........
+  #before_action :confirm_login
   before_action :set_brand, only: [:show, :edit, :update, :destroy]
 
   # GET /brands
